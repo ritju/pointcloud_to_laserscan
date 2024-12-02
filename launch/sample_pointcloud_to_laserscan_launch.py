23f392040098d5156de3b69e9b4f810fc93af603
xx540399793 <540399793@qq.com>
2024-12-02 11:47:30 +0800

    修改深度相机转点云的高度参数

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -6,14 +6,14 @@ from launch_ros.actions import Node
 parameters_back = [{
         'target_frame': 'camera3_color_frame',
         'transform_tolerance': 0.01,
-        'min_height': -0.1,
-        'max_height': 1.8,
+        'min_height': -0.927,
+        'max_height': 0.5,
         'angle_min': -0.614,
         'angle_max': 0.614,
         'angle_increment': 0.00766,
         'scan_time': 0.3333,
         'range_min': 0.3,
-        'range_max': 5.0,
+        'range_max': 10.0,
         'use_inf': True,
         'inf_epsilon': 1.0
     }]
@@ -21,14 +21,14 @@ parameters_back = [{
 parameters_front_up = [{
         'target_frame': 'base_link',
         'transform_tolerance': 0.01,
-        'min_height': -0.1,
-        'max_height': 1.8,
+        'min_height': -0.39,
+        'max_height': 1.0,
         'angle_min': -0.614,
         'angle_max': 0.614,
         'angle_increment': 0.00766,
         'scan_time': 0.3333,
         'range_min': 0.3,
-        'range_max': 5.0,
+        'range_max': 10.0,
         'use_inf': True,
         'inf_epsilon': 1.0
     }]
@@ -36,14 +36,14 @@ parameters_front_up = [{
 parameters_front_down = [{
         'target_frame': 'base_link',
         'transform_tolerance': 0.01,
-        'min_height': -0.5,
-        'max_height': 1.8,
+        'min_height': -0.39,
+        'max_height': 1.0,
         'angle_min': -0.614,
         'angle_max': 0.614,
         'angle_increment': 0.00766,
         'scan_time': 0.3333,
         'range_min': 0.3,
-        'range_max': 5.0,
+        'range_max': 10.0,
         'use_inf': True,
         'inf_epsilon': 1.0
     }]
@@ -58,7 +58,7 @@ node_back = Node(
 
 node_front_up = Node(
     package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-    remappings=[('cloud_in', '/points_cloud_person'),
+    remappings=[('cloud_in', 'camera1/depth/points'),
                 ('scan', 'point_scan_front_up')],
     parameters=parameters_front_up,
     name='pointcloud_to_laserscan_front_up'
@@ -77,25 +77,5 @@ def generate_launch_description():
     return LaunchDescription([
         node_back,
         node_front_up,
-        # node_front_down,
-        # DeclareLaunchArgument(
-        #     name='scanner', default_value='scanner',
-        #     description='Namespace for sample topics'
-        # ),
-        # Node(
-        #     package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
-        #     remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-        #     parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
-        #     name='cloud_publisher'
-        # ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='static_transform_publisher',
-        #     arguments=[
-        #         '--x', '0', '--y', '0', '--z', '0',
-        #         '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
-        #         '--frame-id', 'map', '--child-frame-id', 'cloud'
-        #     ]
-        # ),        
+        node_front_down,   
     ])
