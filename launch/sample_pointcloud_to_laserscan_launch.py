8e9b0ad634f904f60a0f2d62f0f03d0427a8de77
xiaobin0630 <wangxiaobin_hfut@163.com>
2025-04-22 17:22:49 +0800

    设置高度环境变量，适配每台机器人

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -2,12 +2,35 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+import os
+
+if 'BACK_DEPTH_MIN_HEIGHT' and 'BACK_DEPTH_MAX_HEIGHT' in os.environ:
+    back_depth_min_height = float(os.environ.get('BACK_DEPTH_MIN_HEIGHT'))
+    back_depth_max_height = float(os.environ.get('BACK_DEPTH_MAX_HEIGHT'))
+else:
+    back_depth_min_height = -0.927
+    back_depth_max_height = 1.0
+
+if 'FRONT_DOWN_DEPTH_MIN_HEIGHT' and 'FRONT_DOWN_DEPTH_MAX_HEIGHT' in os.environ:
+    front_down_depth_min_height = float(os.environ.get('FRONT_DOWN_DEPTH_MIN_HEIGHT'))
+    front_down_depth_max_height = float(os.environ.get('FRONT_DOWN_DEPTH_MAX_HEIGHT'))
+else:
+    front_down_depth_min_height = -0.41
+    front_down_depth_max_height = 1.0
+
+if 'FRONT_UP_DEPTH_MIN_HEIGHT' and 'FRONT_UP_DEPTH_MAX_HEIGHT' in os.environ:
+    front_up_depth_min_height = float(os.environ.get('FRONT_UP_DEPTH_MIN_HEIGHT'))
+    front_up_depth_max_height = float(os.environ.get('FRONT_UP_DEPTH_MAX_HEIGHT'))
+else:
+    front_up_depth_min_height = -0.4
+    front_up_depth_max_height = 1.8
+
 
 parameters_back = [{
         'target_frame': 'camera3_color_frame',
         'transform_tolerance': 0.01,
         'min_height': -0.927,
-        'max_height': 0.5,
+        'max_height': 1.0,
         'angle_min': -0.614,
         'angle_max': 0.614,
         'angle_increment': 0.00766,
@@ -22,7 +45,7 @@ parameters_front_up = [{
         'target_frame': 'base_link',
         'transform_tolerance': 0.01,
         'min_height': -0.40,
-        'max_height': 1.0,
+        'max_height': 1.8,
         'angle_min': -0.614,
         'angle_max': 0.614,
         'angle_increment': 0.00766,
@@ -36,7 +59,7 @@ parameters_front_up = [{
 parameters_front_down = [{
         'target_frame': 'base_link',
         'transform_tolerance': 0.01,
-        'min_height': -0.35,
+        'min_height': -0.41,
         'max_height': 1.0,
         'angle_min': -0.614,
         'angle_max': 0.614,
