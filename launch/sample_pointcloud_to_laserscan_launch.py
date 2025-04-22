5ae1d4ffc19ca5035ca434b1888c160fb10948f2
xiaobin0630 <wangxiaobin_hfut@163.com>
2025-04-22 15:06:42 +0800

    增加户外机器人分支

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -3,8 +3,8 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-parameters_back = [{
-        'target_frame': 'camera3_color_frame',
+front_camera = [{
+        'target_frame': 'base_link',
         'transform_tolerance': 0.01,
         'min_height': -0.927,
         'max_height': 0.5,
@@ -12,13 +12,13 @@ parameters_back = [{
         'angle_max': 0.614,
         'angle_increment': 0.00766,
         'scan_time': 0.3333,
-        'range_min': 0.3,
+        'range_min': 0.2,
         'range_max': 10.0,
         'use_inf': True,
         'inf_epsilon': 1.0
     }]
 
-parameters_front_up = [{
+right_camera = [{
         'target_frame': 'base_link',
         'transform_tolerance': 0.01,
         'min_height': -0.40,
@@ -27,13 +27,13 @@ parameters_front_up = [{
         'angle_max': 0.614,
         'angle_increment': 0.00766,
         'scan_time': 0.3333,
-        'range_min': 0.3,
+        'range_min': 0.2,
         'range_max': 10.0,
         'use_inf': True,
         'inf_epsilon': 1.0
     }]
 
-parameters_front_down = [{
+back_camera = [{
         'target_frame': 'base_link',
         'transform_tolerance': 0.01,
         'min_height': -0.35,
@@ -42,40 +42,68 @@ parameters_front_down = [{
         'angle_max': 0.614,
         'angle_increment': 0.00766,
         'scan_time': 0.3333,
-        'range_min': 0.3,
+        'range_min': 0.2,
         'range_max': 10.0,
         'use_inf': True,
         'inf_epsilon': 1.0
     }]
 
+left_camera = [{
+        'target_frame': 'base_link',
+        'transform_tolerance': 0.01,
+        'min_height': -0.35,
+        'max_height': 1.0,
+        'angle_min': -0.614,
+        'angle_max': 0.614,
+        'angle_increment': 0.00766,
+        'scan_time': 0.3333,
+        'range_min': 0.2,
+        'range_max': 10.0,
+        'use_inf': True,
+        'inf_epsilon': 1.0
+    }]
+
+
+node_front = Node(
+    package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+    remappings=[('cloud_in', 'camera1/depth/points'),
+                ('scan', 'point_scan_front')],
+    parameters=front_camera,
+    name='pointcloud_to_laserscan_front'
+    )
+
+node_right = Node(
+    package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+    remappings=[('cloud_in', 'camera2/depth/points'),
+                ('scan', 'point_scan_right')],
+    parameters=right_camera,
+    name='pointcloud_to_laserscan_right'
+    )
+
 node_back = Node(
     package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
     remappings=[('cloud_in', 'camera3/depth/points'),
                 ('scan', 'point_scan_back')],
-    parameters=parameters_back,
+    parameters=back_camera,
     name='pointcloud_to_laserscan_back'
     )
 
-node_front_up = Node(
+node_left = Node(
     package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-    remappings=[('cloud_in', 'camera1/depth/points'),
-                ('scan', 'point_scan_front_up')],
-    parameters=parameters_front_up,
-    name='pointcloud_to_laserscan_front_up'
+    remappings=[('cloud_in', 'camera4/depth/points'),
+                ('scan', 'point_scan_left')],
+    parameters=left_camera,
+    name='pointcloud_to_laserscan_left'
     )
 
-node_front_down = Node(
-    package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-    remappings=[('cloud_in', 'camera2/depth/points'),
-                ('scan', 'point_scan_front_down')],
-    parameters=parameters_front_down,
-    name='pointcloud_to_laserscan_front_down'
-    )
+
+
 
 def generate_launch_description():
     
     return LaunchDescription([
+        node_front,
+        node_right,
         node_back,
-        node_front_up,
-        node_front_down,   
+        node_left    
     ])
